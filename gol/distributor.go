1d6f3471ed6976f07c553ee0e087fd3a28cd3fba
qn19325 <qn19325@bristol.ac.uk>
2020-11-17 17:05:39 +0000

    Still no tests passed :(

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,18 +1,51 @@
 package gol
 
+import (
+	"fmt"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
-	events    chan<- Event
-	ioCommand chan<- ioCommand
-	ioIdle    <-chan bool
+	events     chan<- Event
+	ioCommand  chan<- ioCommand
+	ioIdle     <-chan bool
+	ioFilename chan<- string
+	ioOutput   chan<- uint8
+	ioInput    <-chan uint8
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+
 	// TODO: For all initially alive cells send a CellFlipped Event.
 
+	c.ioCommand <- ioCommand(ioInput)                             // send read command down command channel
+	filename := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth) // gets file name from putting file dimensions together
+	c.ioFilename <- filename                                      // sends file name to the fileName channel
+
 	turn := 0
+	aliveCells := make([]util.Cell, 0)   // create aliveCells slice
+	for y := 0; y < p.ImageHeight; y++ { // go through all cells in world
+		for x := 0; x < p.ImageWidth; x++ {
+			// need way to see if current cell is alive
+			val := <-c.ioInput
+			if val != 0 {
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y}) // adds current cell to the aliveCells slice
+				world[y][x] = val                                      // update value of current cell
+			}
+		}
+	}
+
+	for _, cell := range aliveCells {
+		c.events <- CellFlipped{turn, cell} // sends CellFlipped event for all alive cells
+	}
 
 	// TODO: Execute all turns of the Game of Life.
 	// TODO: Send correct Events when required, e.g. CellFlipped, TurnComplete and FinalTurnComplete.
