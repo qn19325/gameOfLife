bccf9066410b222739a768d96cefdda479109476
qn19325 <qn19325@bristol.ac.uk>
2020-11-19 14:19:38 +0000

    current

--- gol/worker.go
+++ gol/worker.go
@@ -4,12 +4,12 @@ func mod(x, m int) int {
 	return (x + m) % m
 }
 
-func aliveNeighbours(world [][]byte, x, y int, p Params) int {
+func aliveNeighbours(world [][]byte, y, x int, p Params) int {
 	neighbours := 0
 	for i := -1; i < 2; i++ {
 		for j := -1; j < 2; j++ {
 			if i != 0 || j != 0 {
-				if world[mod(y+i, p.ImageHeight)][mod(x+j, p.ImageHeight)] != 0 {
+				if world[mod(y+i, p.ImageHeight)][mod(x+j, p.ImageWidth)] != 0 {
 					neighbours++
 				}
 
