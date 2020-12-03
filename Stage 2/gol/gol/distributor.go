46a8832fd92a27c76cde129c15a171173e70349f
Samanimkr <Samanimkr@Outlook.com>
2020-12-03 11:31:48 +0000

    Stage 2 folder setup and controller connects to server 2

---
+++ Stage 2/gol/gol/distributor.go
@@ -0,0 +1,203 @@
+package gol
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+type distributorChannels struct {
+	events                 chan<- Event
+	ioCommand              chan<- ioCommand
+	ioIdle                 <-chan bool
+	ioFilename             chan<- string
+	ioOutput               chan<- uint8
+	ioInput                <-chan uint8
+	keyPresses             <-chan rune
+	keyPressFromController chan string
+}
+
+func handleController(client net.Conn, c distributorChannels) {
+	reader := bufio.NewReader(client)
+	for {
+		keyPress, _ := reader.ReadString('\n')
+		c.keyPressFromController <- keyPress[0:1]
+	}
+}
+
+func worker(world [][]byte, p Params, c distributorChannels, turn int, workerOut chan<- byte, workerHeight int) {
+	tempWorld := createWorld(p.ImageHeight+2, p.ImageWidth)
+
+	for y := 1; y <= workerHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			numAliveNeighbours := aliveNeighbours(world, y, x, p)
+			if world[y][x] == 255 {
+				if numAliveNeighbours == 2 || numAliveNeighbours == 3 {
+					tempWorld[y][x] = 255
+				} else {
+					tempWorld[y][x] = 0
+				}
+			} else {
+				if numAliveNeighbours == 3 {
+					tempWorld[y][x] = 255
+				} else {
+					tempWorld[y][x] = 0
+				}
+			}
+		}
+	}
+	for y := 0; y < workerHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			workerOut <- tempWorld[y+1][x]
+		}
+	}
+}
+
+// distributor divides the work between workers and interacts with other goroutines.
+func distributor(p Params, c distributorChannels, controller net.Conn) {
+
+	// TODO: Create a 2D slice to store the world.
+	world := createWorld(p.ImageHeight, p.ImageWidth)
+
+	// TODO: For all initially alive cells send a CellFlipped Event.
+
+	c.ioCommand <- ioCommand(ioInput)                             // send read command down command channel
+	filename := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth) // gets file name from putting file dimensions together
+	c.ioFilename <- filename                                      // sends file name to the fileName channel
+
+	turn := 0
+
+	currentAliveCells := make([]util.Cell, 0) // create aliveCells slice
+	for y := 0; y < p.ImageHeight; y++ {      // go through all cells in world
+		for x := 0; x < p.ImageWidth; x++ {
+			val := <-c.ioInput
+			if val != 0 {
+				currentAliveCells = append(currentAliveCells, util.Cell{X: x, Y: y}) // adds current cell to the aliveCells slice
+				world[y][x] = 255                                                    // update value of current cell
+			}
+		}
+	}
+
+	for _, cell := range currentAliveCells {
+		c.events <- CellFlipped{turn, cell} // sends CellFlipped event for all alive cells
+	}
+
+	// implement for left over pixels using mod e.g. 256 not divisible by 5 threads
+
+	ticker := time.NewTicker(2 * time.Second)
+
+	for turns := 0; turns < p.Turns; turns++ {
+		select {
+		case pressed := <-c.keyPressFromController:
+			if pressed == "s" {
+				outputPGM(world, c, p, turns)
+			} else if pressed == "q" {
+				outputPGM(world, c, p, turns)
+				c.events <- StateChange{CompletedTurns: turns, NewState: Quitting}
+				c.ioCommand <- ioCheckIdle
+				<-c.ioIdle
+				close(c.events)
+				return
+			} else if pressed == "p" {
+				c.events <- StateChange{CompletedTurns: turns, NewState: Paused}
+				for {
+					tempKey := <-c.keyPressFromController
+					if tempKey == "p" {
+						c.events <- StateChange{CompletedTurns: turns, NewState: Executing}
+						break
+					}
+				}
+			}
+		case <-ticker.C:
+			aliveCellsNum := 0
+			for y := 0; y < p.ImageHeight; y++ {
+				for x := 0; x < p.ImageWidth; x++ {
+					if world[y][x] == 255 {
+						aliveCellsNum++
+					}
+				}
+			}
+
+			c.events <- AliveCellsCount{
+				CompletedTurns: turns,
+				CellsCount:     aliveCellsNum,
+			}
+
+			fmt.Fprintln(controller, "AliveCells: "+strconv.Itoa(aliveCellsNum))
+		default:
+		}
+
+		workerOut := make([]chan byte, p.Threads)
+		workerHeight := p.ImageHeight / p.Threads
+		remainderHeight := p.ImageHeight % p.Threads
+
+		for thread := 0; thread < p.Threads; thread++ {
+			var currentSplit [][]byte
+
+			workerHeightWithRemainder := workerHeight + remainderHeight
+			workerOut[thread] = make(chan byte)
+
+			currentSplit = splitWorld(world, workerHeight, workerHeightWithRemainder, thread, turns, p)
+
+			if thread == p.Threads-1 {
+				go worker(currentSplit, p, c, turns, workerOut[thread], workerHeightWithRemainder)
+			} else {
+				go worker(currentSplit, p, c, turns, workerOut[thread], workerHeight)
+			}
+
+		}
+		for thread := 0; thread < p.Threads; thread++ {
+			var splitHeight int
+			if thread == (p.Threads - 1) {
+				splitHeight = workerHeight + remainderHeight
+			} else {
+				splitHeight = workerHeight
+			}
+			newSplit := make([][]byte, splitHeight)
+			for i := range newSplit {
+				newSplit[i] = make([]byte, p.ImageWidth)
+			}
+
+			for y := 0; y < splitHeight; y++ {
+				for x := 0; x < p.ImageWidth; x++ {
+					newSplit[y][x] = <-workerOut[thread]
+				}
+			}
+			for y := 0; y < splitHeight; y++ {
+				for x := 0; x < p.ImageWidth; x++ {
+					worldY := thread*workerHeight + y
+
+					if world[worldY][x] != newSplit[y][x] {
+						world[worldY][x] = newSplit[y][x]
+						c.events <- CellFlipped{turn, util.Cell{Y: worldY, X: x}}
+					}
+				}
+			}
+		}
+		c.events <- TurnComplete{turns}
+	}
+
+	finalAliveCells := make([]util.Cell, 0)
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == 255 {
+				cell := util.Cell{Y: y, X: x}
+				finalAliveCells = append(finalAliveCells, cell)
+			}
+		}
+	}
+
+	c.events <- FinalTurnComplete{p.Turns, finalAliveCells}
+
+	outputPGM(world, c, p, p.Turns)
+
+	c.ioCommand <- ioCheckIdle
+	<-c.ioIdle
+
+	c.events <- StateChange{p.Turns, Quitting}
+	close(c.events)
+}
