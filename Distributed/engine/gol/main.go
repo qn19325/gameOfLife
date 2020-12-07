1de815594d4812325cdccc5475bbacd4f4e9aa3a
Jared Hall <jared@Jareds-MacBook-Air.local>
2020-12-07 13:33:39 +0000

    updated to rpc connection engine.main

--- Distributed/engine/gol/main.go
+++ Distributed/engine/gol/main.go
@@ -2,31 +2,46 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net"
+	"net/rpc"
 	"runtime"
 )
 
+type Engine struct {
+}
+type args struct {
+	world [][]byte
+	turn  int
+	p     Params
+	c     distributorChannels
+}
+
 // -----Run function-----
 // starts the distributor function with world and number of turns
+func (engine *Engine) Run(args Args, reply *[][]byte) error {
+	go distributor(args.p, args.c, args.world)
+}
 
-// function to get the world
+// function to send current world to client
+func (engine *Engine) getCurrentWorld()
 
-// function to get number of turns specified in controller
+// function to get current turn
 
 // main is the function called when starting Game of Life with 'go run .'
 func main() {
-	// -----THINK NEED USE RPC IN HERE-----
+	// -----THIS MAY BE WRONG-----
 
-	runtime.LockOSThread()
+	runtime.LockOSThread() // not sure what this does but was in skeleton
 	// Port for connection to controller
-	portPtr := flag.String("port", ":8030", "port to listen on")
-	flag.Parse()
-
-	ln, _ := net.Listen("tcp", *portPtr)
-	// handleError(err)
-
-	// Get connection
-	connection, _ := ln.Accept()
-	// handleError(err)
+	portPtr := flag.String("port", ":8030", "listening on this port")
+	flag.Parse()                             // call after all flags are defined to parse command line into flags
+	rpc.Register(&Engine)                    // WHAT DOES THIS DO?
+	ln, error := net.Listen("tcp", *portPtr) // listens for connections
+	if error != nil {                        // produces error message if fails to connect
+		log.Fatal("Unable to connect:", error)
+	}
+	defer ln.Close() // stops execution until surrounding functions return
+	rpc.Accept(ln)   // accepts connections on ln and serves requests to server for each incoming connection
 
 }
