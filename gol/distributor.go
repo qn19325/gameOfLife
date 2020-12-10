1d6e0f7b69a2fa82276561305014b452fab1e55a
Samanimkr <Samanimkr@Outlook.com>
2020-12-10 01:05:16 +0000

    Comments all the code

--- gol/distributor.go
+++ gol/distributor.go
@@ -17,14 +17,21 @@ type distributorChannels struct {
 	keyPresses <-chan rune
 }
 
+// WorkerStartHeight is a global variable for the current worker's starting height
 var WorkerStartHeight int
 
+// Worker function that calculates the new state of each cell within it's boundaries
 func worker(world [][]byte, p Params, c distributorChannels, turn int, workerOut chan<- byte, workerHeight int) {
+	// Create a temporary empty world
 	tempWorld := createWorld(p.ImageHeight+2, p.ImageWidth)
 
+	// Loop through the worker's section of the world
 	for y := 1; y <= workerHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
+			// Get number of alive neighbours
 			numAliveNeighbours := aliveNeighbours(world, y, x, p)
+
+			// Calculate what's the new state of the cell depending on alive neighbours
 			if world[y][x] == 255 {
 				if numAliveNeighbours == 2 || numAliveNeighbours == 3 {
 					tempWorld[y][x] = 255
@@ -40,6 +47,8 @@ func worker(world [][]byte, p Params, c distributorChannels, turn int, workerOut
 			}
 		}
 	}
+
+	// Send the updated world down the 'workerOut' channel
 	for y := 0; y < workerHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			workerOut <- tempWorld[y+1][x]
@@ -50,60 +59,70 @@ func worker(world [][]byte, p Params, c distributorChannels, turn int, workerOut
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
-	// TODO: Create a 2D slice to store the world.
+	// Create a 2D slice to store the world.
 	world := createWorld(p.ImageHeight, p.ImageWidth)
 
-	// TODO: For all initially alive cells send a CellFlipped Event.
+	// Send read command down command channel
+	c.ioCommand <- ioCommand(ioInput)
+	// Gets file name from putting file dimensions together
+	filename := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth)
+	// Sends file name to the fileName channel
+	c.ioFilename <- filename
 
-	c.ioCommand <- ioCommand(ioInput)                             // send read command down command channel
-	filename := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth) // gets file name from putting file dimensions together
-	c.ioFilename <- filename                                      // sends file name to the fileName channel
-
-	turn := 0
-
-	currentAliveCells := make([]util.Cell, 0) // create aliveCells slice
-	for y := 0; y < p.ImageHeight; y++ {      // go through all cells in world
+	// Calculate which cells are alive at the start
+	currentAliveCells := make([]util.Cell, 0)
+	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			val := <-c.ioInput
 			if val != 0 {
-				currentAliveCells = append(currentAliveCells, util.Cell{X: x, Y: y}) // adds current cell to the aliveCells slice
-				world[y][x] = 255                                                    // update value of current cell
+				// Adds current cell to the aliveCells slice
+				currentAliveCells = append(currentAliveCells, util.Cell{X: x, Y: y})
+				// Update value of current cell
+				world[y][x] = 255
 			}
 		}
 	}
 
+	// For all initially alive cells send a CellFlipped Event.
 	for _, cell := range currentAliveCells {
-		c.events <- CellFlipped{turn, cell} // sends CellFlipped event for all alive cells
+		c.events <- CellFlipped{0, cell}
 	}
 
-	// implement for left over pixels using mod e.g. 256 not divisible by 5 threads
-
+	// Create a new ticker that goes off every 2 seconds
 	ticker := time.NewTicker(2 * time.Second)
 
-	for turns := 0; turns < p.Turns; turns++ {
-
+	// Loop through all the turns for this game
+	for turn := 0; turn < p.Turns; turn++ {
 		select {
+		// If a key is pressed
 		case pressed := <-c.keyPresses:
 			if pressed == 's' {
-				outputPGM(world, c, p, turns)
+				// If 's' is pressed then save the current world as a pgm file
+				outputPGM(world, c, p, turn)
 			} else if pressed == 'q' {
-				outputPGM(world, c, p, turns)
-				c.events <- StateChange{CompletedTurns: turns, NewState: Quitting}
+				// If 'q' is pressed
+				// then save the current world as a pgm file
+				outputPGM(world, c, p, turn)
+
+				// Quit the execution
+				c.events <- StateChange{CompletedTurns: turn, NewState: Quitting}
 				c.ioCommand <- ioCheckIdle
 				<-c.ioIdle
 				close(c.events)
 				return
 			} else if pressed == 'p' {
-				c.events <- StateChange{CompletedTurns: turns, NewState: Paused}
+				// If 'p' is pressed, then pause the game until 'p' is pressed again
+				c.events <- StateChange{CompletedTurns: turn, NewState: Paused}
 				for {
 					tempKey := <-c.keyPresses
 					if tempKey == 'p' {
-						c.events <- StateChange{CompletedTurns: turns, NewState: Executing}
+						c.events <- StateChange{CompletedTurns: turn, NewState: Executing}
 						break
 					}
 				}
 			}
 		case <-ticker.C:
+			// When the ticker ticks, find out the number of alive cells in the world
 			aliveCellsNum := 0
 			for y := 0; y < p.ImageHeight; y++ {
 				for x := 0; x < p.ImageWidth; x++ {
@@ -112,23 +131,26 @@ func distributor(p Params, c distributorChannels) {
 					}
 				}
 			}
-
+			// Notify the user about the number of alive cells
 			c.events <- AliveCellsCount{
-				CompletedTurns: turns,
+				CompletedTurns: turn,
 				CellsCount:     aliveCellsNum,
 			}
 		default:
 		}
 
+		// A channel for sending the updated world
 		workerOut := make([]chan byte, p.Threads)
+		// Height of each worker
 		workerHeight := p.ImageHeight / p.Threads
+		// Height of the remainder that isn't inluded in each worker
 		remainderHeight := p.ImageHeight % p.Threads
 		WorkerStartHeight = 0
 
 		for thread := 0; thread < p.Threads; thread++ {
-			var currentSplit [][]byte
 			workerOut[thread] = make(chan byte)
 
+			// Spread the remainder height across the first few threads
 			var splitHeight int
 			if remainderHeight > 0 {
 				splitHeight = workerHeight + 1
@@ -136,14 +158,22 @@ func distributor(p Params, c distributorChannels) {
 				splitHeight = workerHeight
 			}
 
-			currentSplit = splitWorld(world, splitHeight, thread, turns, p)
-			go worker(currentSplit, p, c, turns, workerOut[thread], splitHeight)
+			// Split the world up into similar sized sections and receive the current split
+			currentSplit := splitWorld(world, splitHeight, thread, turn, p)
+			// Send that section to the worker to be processed
+			go worker(currentSplit, p, c, turn, workerOut[thread], splitHeight)
 
+			// Increment the WorkerStartHeight so it's ready for the next section
 			WorkerStartHeight += splitHeight
-			remainderHeight--
+			// Decrement the remainderHeight
+			if remainderHeight > 0 {
+				remainderHeight--
+			}
 		}
+		// Reset the remainderHeight and WorkerStartHeight values
 		remainderHeight = p.ImageHeight % p.Threads
 		WorkerStartHeight = 0
+
 		for thread := 0; thread < p.Threads; thread++ {
 			var splitHeight int
 			if remainderHeight > 0 {
@@ -151,29 +181,33 @@ func distributor(p Params, c distributorChannels) {
 			} else {
 				splitHeight = workerHeight
 			}
+			// Create an empty 2D slice for this thread's worker
 			newSplit := createWorld(splitHeight, p.ImageWidth)
 
+			// Get all the updated cells from the 'workerOut' channel
 			for y := 0; y < splitHeight; y++ {
 				for x := 0; x < p.ImageWidth; x++ {
+					// Get the new value
 					newSplit[y][x] = <-workerOut[thread]
-				}
-			}
-			for y := 0; y < splitHeight; y++ {
-				for x := 0; x < p.ImageWidth; x++ {
-					worldY := WorkerStartHeight + y
 
+					// Update that cell in 'world'
+					worldY := WorkerStartHeight + y
 					if world[worldY][x] != newSplit[y][x] {
 						world[worldY][x] = newSplit[y][x]
 						c.events <- CellFlipped{turn, util.Cell{Y: worldY, X: x}}
 					}
 				}
 			}
+
 			remainderHeight--
 			WorkerStartHeight += splitHeight
 		}
-		c.events <- TurnComplete{turns}
+
+		// Send an event for completing that turn
+		c.events <- TurnComplete{turn}
 	}
 
+	// Calculate the final alive cells
 	finalAliveCells := make([]util.Cell, 0)
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
@@ -184,13 +218,12 @@ func distributor(p Params, c distributorChannels) {
 		}
 	}
 
+	// Send an event telling the SDL the new state of the board
 	c.events <- FinalTurnComplete{p.Turns, finalAliveCells}
-
+	// Output the world into a PGM image
 	outputPGM(world, c, p, p.Turns)
 
-	c.ioCommand <- ioCheckIdle
-	<-c.ioIdle
-
+	// Quit the execution
 	c.events <- StateChange{p.Turns, Quitting}
 	close(c.events)
 }
--- gol/helper.go
+++ gol/helper.go
@@ -6,6 +6,7 @@ func mod(x, m int) int {
 	return (x + m) % m
 }
 
+// Creates a 2D slice of the world depending on inputted height and width
 func createWorld(height, width int) [][]byte {
 	world := make([][]byte, height)
 	for i := range world {
@@ -14,6 +15,7 @@ func createWorld(height, width int) [][]byte {
 	return world
 }
 
+// Calculate the number of alive neighbours around the cell
 func aliveNeighbours(world [][]byte, y, x int, p Params) int {
 	neighbours := 0
 	for i := -1; i < 2; i++ {
@@ -29,17 +31,23 @@ func aliveNeighbours(world [][]byte, y, x int, p Params) int {
 	return neighbours
 }
 
+// Split the world into the dimensions specified
 func splitWorld(world [][]byte, splitHeight, currentThread, turn int, p Params) [][]byte {
 	tempWorld := createWorld(splitHeight+2, p.ImageWidth)
 
+	// Set the previous row in tempWorld
 	for x := 0; x < p.ImageWidth; x++ {
 		previousRow := (WorkerStartHeight + p.ImageHeight - 1) % p.ImageHeight
 		tempWorld[0][x] = world[previousRow][x]
 	}
+
+	// Set the next row in tempWorld
 	for x := 0; x < p.ImageWidth; x++ {
 		nextRow := (WorkerStartHeight + splitHeight + p.ImageHeight) % p.ImageHeight
 		tempWorld[splitHeight+1][x] = world[nextRow][x]
 	}
+
+	// Set all the middle rows in tempWorld
 	for y := 1; y <= splitHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			currentRow := WorkerStartHeight + y - 1
@@ -49,15 +57,18 @@ func splitWorld(world [][]byte, splitHeight, currentThread, turn int, p Params) 
 	return tempWorld
 }
 
+// Save the current state of the world as a PGM file
 func outputPGM(world [][]byte, c distributorChannels, p Params, turn int) {
 	c.ioCommand <- ioCommand(ioOutput)
 	outputFileName := fmt.Sprintf("%dx%dx%d", p.ImageHeight, p.ImageWidth, turn)
 	c.ioFilename <- outputFileName
+
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			c.ioOutput <- world[y][x]
 		}
 	}
+
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 	c.events <- ImageOutputComplete{turn, outputFileName}
