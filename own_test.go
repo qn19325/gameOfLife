742a04584e48dfb7b8b6c999e0aa2168dedf4ef8
Euan Bagguley <euanbagguley@Euans-MacBook-Pro.local>
2020-12-11 04:53:53 +0000

    final parrallel implementation

--- own_test.go
+++ own_test.go
@@ -3,31 +3,30 @@ package main
  
 import (
     "fmt"
-    "os"
-    "testing"
- 
     "uk.ac.bris.cs/gameoflife/gol"
+    "testing"
+    "os"
 )
  
 func Benchmark(b *testing.B) {
 
-    tests := []gol.Params{
+    dimensions := []gol.Params{
         {ImageWidth: 16, ImageHeight: 16},
         {ImageWidth: 128, ImageHeight: 128},
         {ImageWidth: 512, ImageHeight: 512},
     }
-    for _, p := range tests {
+    for _, params := range dimensions {
         for i:=0; i<3; i++{
-            p.Threads = 16
-            p.Turns = 100
+            params.Threads = 16
+            params.Turns = 100
             os.Stdout = nil
-            testName := fmt.Sprintf("Testing Benchmark For: %dx%d Turns: %d  Threads: %d ", p.ImageHeight, p.ImageWidth, p.Turns, p.Threads)
+            testName := fmt.Sprintf("Testing Benchmark For: %dx%d Turns: %d  Threads: %d ", params.ImageHeight, params.ImageWidth, params.Turns, params.Threads)
             b.Run(testName, func(b *testing.B) {
                 for i := 0; i < b.N; i++ {
-                    events := make(chan gol.Event)
-                    gol.Run(p, events, nil)
-                    for range events{
-
+                    events := make(chan gol.Event,1000)
+                    keypresses := make(chan rune, 10)
+                    gol.Run(params, events, keypresses)
+                    for range events{                      
                     }
                     
                 }
